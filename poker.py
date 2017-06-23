c01c0065eae38eb5cf1d14b615bf628bd6a0bd52
Shane Hall <cyrus.s.hall84@gmail.com>
2017-06-23 13:38:27 -0400

    6-23

---
+++ poker.py
@@ -0,0 +1,26 @@
+var J = 11;
+var Q = 12;
+var K = 13;
+var A = 14;
+
+
+
+var highCard = 1;
+var pair = 2;
+var twoPair = 3;
+var threeOfAKind = 4;
+var straight = 5;
+var flush = 6;
+var fullHouse = 7;
+var fourOfAKind = 8;
+var straightFlush = 9;
+var royalFlush = 10;
+
+var faceCards {
+    J : 11,
+    Q : 12,
+    K : 13,
+    A : 14
+}
+
+
